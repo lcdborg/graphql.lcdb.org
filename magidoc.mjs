6108d573d6ca8752cb335af4b4dc351d32c0f244
Tom H Anderson <tom.h.anderson@gmail.com>
2024-03-27 10:01:09 -0600

    New docs builds with 10.0.4

--- magidoc.mjs
+++ magidoc.mjs
@@ -20,7 +20,7 @@ LCDB GraphQL Database
 
 This is the graph database for \`https://lcdb.org\`.
 
-You are free to use this GraphQL database for your own purposes.
+You are free to use this database for your own purposes.
 
 The endpoint is a POST to \`https://graphql.lcdb.org\`
           `,
